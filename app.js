faf7f374c607447147cbb73d863b2d6c49cad2ca
unknown <johnloydlegend@gmail.com>
2023-04-11 14:02:54 +0100

    Added express_middleware class code

--- app.js
+++ app.js
@@ -6,9 +6,38 @@ const database = {}
 const app = express()
 const port = 4000
 
+app.use(express.json())
+
+
+const users = [
+{
+  name: 'John',
+  sex: 'male',
+  age: 46
+},
+{
+  name: 'Anne',
+  sex: 'female',
+  age:78
+},
+{
+  name: 'Prosper',
+  sex: 'male',
+  age: 40
+},
+{
+  name: 'Ada',
+  sex: 'female',
+  age: 19
+}
+]
+
+
+
+
 // app.use(bodyParser())
 
-app.get('/', (req, res) => {
+app.delete('/:id', (req, res) => {
   return res.send('HELLO WORLD!')
 })
 
@@ -16,11 +45,60 @@ app.post('/new_post', (req, res) => {
   console.log(req.body)
   const newPostDate = req.body
   database['new_post'] = newPostDate
-  res.send('New Tweet saved')
+  res.json('New Tweet saved')
 })
 
-app.get('/home', (req, res) => {
-  return res.send('HELLO HOME PAGE!')
+const getUserController =  (req, res) => {
+  const name = req.params.name
+
+  if(!name) return res.status(404).json({
+    status: 'failed',
+    message: 'Please pass in a user name',
+  })
+
+  const user = users.find(user => user.name.toLowerCase() == name.toLowerCase())
+
+  if(!user) return res.status(404).json({
+    status: 'failed',
+    message: 'User not found',
+  })
+
+  return res.json({
+    status: 'success',
+    message: 'User found',
+    user
+  })
+}
+// This is the endpoing that will return a single user once their name is matched.
+
+app.get('/user/:name', getUserController)
+
+const createUserController =  (req, res) => {
+  const userObject = req.body
+  users.push(userObject)
+  res.status(201).json({
+    status: 'success',
+    message: 'User Created',
+    users
+  })
+}
+
+
+app.post('/user', createUserController)
+
+
+app.get('/users', (req, res) => {
+  res.json({
+    status: 'success',
+    message: users.length < 1 ? "Users not found" :  "Users found",
+    users
+  })
+})
+
+
+
+app.post('/home', (req, res) => {
+  return res.send('HELLO HOME PAGE! FROM THE POST ROUTE')
 })
 
 
